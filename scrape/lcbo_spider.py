f04fd394f8e20094a25f29a9e8dc1d0761fbd3ac
Yoyotruly <yoyo.truly@gmail.com>
2019-06-17 23:52:42 -0400

    Modify scraping code

--- scrape/lcbo_spider.py
+++ scrape/lcbo_spider.py
@@ -9,84 +9,93 @@ import pandas as pd
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-LIST = {
-    'Name': [],
-    'Price': [],
-    'Product URL': []
+listing = {
+    'name': [],
+    'price': [],
+    'prod_url': []
 }
 
-PRODUCT = {
-    'Product URL': [],
-    'SKU': [],
-    'Category': [],
-    'Description': []
-#     'Bottle Size': [],
-#     'Alcohol/Vol': [],
-#     'Made In': [],
-#     'By': [],
-#     'Sugar Content': [],
-#     'Sweetness Descriptor': [],
-#     'Style': [],
-#     'Varietal': []
+product = {
+    'url': [],
+    'sku': [],
+    'category': [],
+    'description': [],
+    'attributes': [],
+    'values': []
 }
 
 class LCBOSpider(scrapy.Spider):
     name = 'lcbo_spider'
 
-    def start_requests(self):
-        url = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14'
-        yield scrapy.Request(url=url, callback=self.parse_list)
+    def __init__(self):
+        self.last_page = None
 
-    def parse_list(self, response):
+    def start_requests(self):
+        urls = [
+            'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14/red-wine-14001',
+            'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14/white-wine-14002',
+            'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14/ros%C3%A9-wine-14003',
+            'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14/champagne-14004',
+            'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/wine-14/sparkling-wine-14005'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_listing)
+
+    def parse_listing(self, response):
         names = response.css('.product_name a::text').extract()
         prices = response.css('#content .price::text').extract()
         prod_urls = response.css('.product_name a::attr(href)').extract()
 
         for name in names:
-            LIST['Name'].append(name)
+            listing['name'].append(name)
 
         for price in prices:
             try:
-                LIST['Price'].append(float(re.sub(r'[$|,]', '', price.strip())))
+                listing['price'].append(float(re.sub(r'[$|,]', '', price.strip())))
             except ValueError:
-                LIST['Price'].append(np.nan)
+                listing['price'].append(np.nan)
 
         for prod_url in prod_urls:
             if prod_url:
-                LIST['Product URL'].append(prod_url)
+                listing['prod_url'].append(prod_url)
                 yield response.follow(url=prod_url, callback=self.parse_product)
             else:
-                LIST['Product URL'].append(np.nan)
+                listing['prod_url'].append(np.nan)
 
-        current_page = int(response.css('#content > div.right.col-md-9.col-xs-12.col-sm-9 > div.productListingWidget > div.header_bar.topFieldBar > div.controls.pagination_present > div.paging_controls > div > div > div > div > a::text').extract_first())
-        if current_page <= 831:
+        page_num = int(response.css('#content > div.right.col-md-9.col-xs-12.col-sm-9 > div.productListingWidget > div.header_bar.topFieldBar > div.controls.pagination_present > div.paging_controls > div > div > div > div > a::text').extract_first())
+
+        if page_num == 1:
+            last_page_num = int(response.css('.ellipsis+ .hoverover::text').extract_first())
+            self.last_page = last_page_num
+
+        if page_num <= self.last_page:
             next_page_url = response.css('#WC_SearchBasedNavigationResults_pagination_link_right_categoryResults::attr(href)').extract_first()
-            yield response.follow(url=next_page_url, callback=self.parse_list)
+            yield response.follow(url=next_page_url, callback=self.parse_listing)
 
     @classmethod
     def parse_product(cls, response):
         sku = response.css('#prodSku span+ span::text').extract_first()
         category = response.css('.headingNickname::text').extract_first()
         description = response.css('#contentWrapper .hidden-xs::text').extract_first()
-        # prod_keys = response.css('.product-details-list b::text').extract()
-        # prod_values = response.css('.product-details-list span::text').extract()
+        attributes = response.css('.product-details-list b::text').extract()
+        values = response.css('.product-details-list span::text').extract()
 
-        PRODUCT['Product URL'].append(response.url)
+        product['url'].append(response.url)
+        product['category'].append(category)
+        product['description'].append(description)
+        product['attributes'].append(attributes)
+        product['values'].append(values)
+
         if sku:
-            PRODUCT['SKU'].append(sku.strip())
+            product['sku'].append(sku.strip())
         else:
-            PRODUCT['SKU'].append(np.nan)
-        PRODUCT['Category'].append(category)
-        PRODUCT['Description'].append(description)
-
-        # for i in range(len(prod_keys)):
-        #     RESULT[prod_keys[i][:-1]].append(prod_values[i])
+            product['sku'].append(np.nan)
 
 
-process = CrawlerProcess()
-process.crawl(LCBOSpider)
-process.start()
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(LCBOSpider)
+    process.start()
 
-
-pd.DataFrame(LIST).to_csv('lcbo_list.csv', index=False)
-pd.DataFrame(PRODUCT).to_csv('lcbo_product.csv', index=False)
+    pd.DataFrame(listing).to_csv('lcbo_listing.csv', index=False)
+    pd.DataFrame(product).to_csv('lcbo_product.csv', index=False)
