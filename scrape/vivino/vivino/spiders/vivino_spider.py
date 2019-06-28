2b093b3bff659bdc2297263520e4dd773c6a36c9
Yoyotruly <yoyo.truly@gmail.com>
2019-06-28 00:30:14 -0400

    Fix parsing methods and storing mechanism

--- scrape/vivino/vivino/spiders/vivino_spider.py
+++ scrape/vivino/vivino/spiders/vivino_spider.py
@@ -1,15 +1,15 @@
 import json
 import math
-import pandas as pd
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from vivino.items import ProductItem, ReviewItem
 
 
 class VivinoSpider(scrapy.Spider):
     '''
-    add docstring
+    A web crawler that scrapes vivino.com for red, white, rose and sparkling
+    wine information and their corresponding user reviews.
     '''
-
     name = 'vivino_spider'
 
     def __init__(self):
@@ -20,66 +20,92 @@ class VivinoSpider(scrapy.Spider):
         self.review_page_num = 1
 
     def start_requests(self):
-        url = self.prod_url.format(self.prod_page_num)
+        url = self.prod_url.format(1)
         yield scrapy.Request(url=url, callback=self.parse_product)
 
     def parse_product(self, response):
         '''
-        add docstring
+        Extract information about each one and store result in ProductItem.
+        Yield scrapy request following individual product url and send response
+        to parse_review.
         '''
-
         data = json.loads(response.body).get('explore_vintage')
-        products = data.get('records')
+        records = data.get('records')
 
-        for product in products:
-            print({
-                'ratings_ave': products.get('vintage').get('statistics').get('ratings_average'),
-                'wine_id': product.get('wine').get('id'),
-                'name': product.get('wine').get('name'),
-                'seo_name': product.get('wine').get('seo_name'),
-                'wine_type': product.get('wine').get('type_id'),
-                'region': product.get('wine').get('region'),
-                'winery': product.get('wine').get('winery').get('name'),
-                'taste': product.get('wine').get('taste'),
-                'style': product.get('wine').get('style'),
-                'year': product.get('year'),
-                'price': product.get('price').get('amount')
-            })
+        # extract product information and store result in scrapy item
+        for record in records:
+            product = ProductItem()
+            year = record.get('vintage').get('year')
+            wine = record.get('vintage').get('wine')
+            wine_id = wine.get('id')
 
+            try:
+                product['wine_id'] = wine_id
+                product['name'] = wine.get('name')
+                product['seo_name'] = wine.get('seo_name')
+                product['type'] = wine.get('type_id')
+                product['region'] = wine.get('region').get('name')
+                product['country'] = wine.get('region').get('country').get('name')
+                product['winery'] = wine.get('winery').get('name')
+                product['flavor'] = wine.get('taste').get('flavor')
+                product['structure'] = wine.get('taste').get('structure')
+                product['style'] = wine.get('style').get('name')
+                product['body'] = wine.get('style').get('body')
+                product['acidity'] = wine.get('style').get('acidity')
+                product['ratings_ave'] = record.get('vintage').get('statistics').get('ratings_average')
+                product['year'] = year
+                product['price'] = record.get('price').get('amount')
+            except (AttributeError, KeyError):
+                pass
+
+            yield product
+
+            # follow product page url and send response to parse_review
+            if year == 'N.V.':
+                review_url = self.review_url.format(wine_id, 'null') % 1
+            else:
+                review_url = self.review_url.format(wine_id, year) % 1
+            yield scrapy.Request(url=review_url, callback=self.parse_review)
+
+
+        # find total number of records matched and calculate max page number
+        # (vivino.com uses infinite scrolling, therefore no pagination)
         if self.prod_page_num == 1:
             self.prod_page_last = math.ceil(data['records_matched']/25)
 
+        # keep following link until reach last page
         if self.prod_page_num < self.prod_page_last:
             self.prod_page_num += 1
             prod_url_next = self.prod_url.format(self.prod_page_num)
-            yield scrapy.Request(url=prod_url_next,
-                                 callback=self.parse_product)
+            yield scrapy.Request(url=prod_url_next, callback=self.parse_product)
 
-        for wine_id, year in wine_id, year:
-            review_url = self.review_url.format(wine_id, year) % self.review_page_num
-            yield scrapy.Request(url=review_url,
-                                 callback=self.parse_review)
 
     def parse_review(self, response):
         '''
-        add docstring
+        Extract user reviews for each wine and store results in ReviewItem.
+        Keep scraping while page content is not empty.
         '''
         reviews = json.loads(response.body).get('reviews')
 
-        for review in reviews:
-            print({
-                'user_id': review.get('id'),
-                'rating': review.get('rating'),
-                'note': review.get('note'),
-                'language': review.get('language'),
-                'created_at': review.get('created_at'),
-                'wine_id': review.get('vintage').get('wine').get('id'),
-                'year': review.get('vintage').get('year')
-            })
+        for rev in reviews:
+            review = ReviewItem()
+            wine_id = rev.get('vintage').get('wine').get('id')
+            year = rev.get('vintage').get('year')
+            review['wine_id'] = wine_id
+            review['year'] = year
+            review['user_id'] = rev.get('id')
+            review['rating'] = rev.get('rating')
+            review['note'] = rev.get('note')
+            review['language'] = rev.get('language')
+            review['created_at'] = rev.get('created_at')
+            yield review
 
-        while not reviews:
+        if reviews:
             self.review_page_num += 1
-            review_url_next = self.review_url.format(wine_id, year) % self.review_page_num
+            if year == 'N.V.':
+                review_url_next = self.review_url.format(wine_id, 'null') % self.review_page_num
+            else:
+                review_url_next = self.review_url.format(wine_id, year) % self.review_page_num
             yield scrapy.Request(url=review_url_next,
                                  callback=self.parse_review)
 
@@ -88,6 +114,3 @@ if __name__ == '__main__':
     process = CrawlerProcess()
     process.crawl(VivinoSpider)
     process.start()
-
-    pd.DataFrame().to_csv('vivino_product.csv', index=False)
-    pd.DataFrame().to_csv('vivino_review.csv', index=False)
