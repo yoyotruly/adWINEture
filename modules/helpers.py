b9e8ae7b39c4511e936b862f8319d8e3b4678a5a
Yoyotruly <yoyo.truly@gmail.com>
2019-07-04 01:42:22 -0400

    Add helper functions module

---
+++ modules/helpers.py
@@ -0,0 +1,35 @@
+import re
+import unidecode
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+def clean_text(text):
+
+    """
+    Convert text to all lowercase and strip it of natural line breaks, non-
+    unicode characters, punctuations, numbers, unwanted regex patterns, and
+    stopwords, then lemmatize nouns and verbs.
+
+    Args:
+    text: a string.
+
+    Returns:
+    A cleaned text string.
+    """
+
+    bad_regex = [r'\d+', r'[^\w\s]', r'\([A-Z].*?\d\)', r'\S+.com']
+    stop_words = stopwords.words('english')
+    wnl = WordNetLemmatizer()
+
+    text = text.lower()
+    text = ' '.join(text.splitlines())  # remove natural line breaks
+    text = unidecode.unidecode(text)  # remove non-English characters
+    text = re.sub('|'.join(bad_regex), '', text) # remove unwanted regex patterns
+
+    tokens = word_tokenize(text)
+    tokens = [wnl.lemmatize(token) for token in tokens if token not in stop_words]  # lemmatize and remove stopwords
+    text = ' '.join(tokens)
+
+    return text
